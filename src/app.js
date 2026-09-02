fe86bfaa4a093afcdfcd43639b44e5bd9cfdf440
Rushikesh <rvp6123@gmail.com>
2026-09-02 23:02:03 +0530

    feat: add cookie-parser and jsonwebtoken for authentication and token management

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 
 const AuthRoutes = require('./routes/auth.routes');
 
 const app = express();
-app.use(express.json())
 
-app.use("/")
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/", AuthRoutes)
 
 module.exports = app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const userModel = require('../models/user.model');
-
+const jwt = require('jsonwebtoken');
 
 /** 
 * - User Registration Controller
@@ -22,6 +22,17 @@ async function userRegisterController(req, res) {
 
     const user = await userModel.create({
         email, password, name
+    })
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+    res.cookie("token", token)
+    res.status(201).json({
+        user:{
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token 
     })
 
 
