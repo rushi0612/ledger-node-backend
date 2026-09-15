c9222a0450f8d321e211db796e2ade6754623ba6
Rushikesh <rvp6123@gmail.com>
2026-09-15 22:14:31 +0530

    feat: add transaction model with schema for handling transactions

---
+++ src/models/transaction.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+
+const transactionSchema = new mongoose.Schema({
+    
+    fromAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"account",
+        required:[ true, "Transaction must be associated with a from account"],
+        index:true
+    },
+    toAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"account",
+        required:[ true, "Transaction must be associated with a to account"],
+        index:true
+    },
+    status:{
+        type:String,
+        enum:{
+            values:["PENDING", "COMPLETE", "FAILED", "REVERSED"],
+            message:"Status can be either PENDING, COMPLETE, FAILED or REVERCED",
+        },
+        default:"PENDING"
+    },
+    amount:{
+        type:Number,
+        required:[ true, "Amount is required for creacting transaction"],
+        min: [ 0, "Transaction amount cannot be in negative"]
+    },
+    idempotencyKey:{
+        type: String,
+        required: [ true, "Idempotency key is required to creating transaction"],
+        index: true,
+        unique: true,
+    }
+
+},
+    {
+        timestamps:true
+    }
+)
+
+
+const transactionModel = mongoose.model("transaction", transactionSchema);
+
+module.exports = transactionModel;
